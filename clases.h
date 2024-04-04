616328e0a3cd32daf70596d3901d2430de76837d
Davkcy <164350888+Davkcy@users.noreply.github.com>
2024-04-04 07:41:56 -0500

    Update clases.h

---
+++ clases.h
@@ -0,0 +1,20 @@
+#include <string>
+using namespace std;
+
+class Student {
+public:
+  explicit Student(string n, int a);
+  Student() : name(""), age(0) {}
+
+  void setName(string n) { name = n; }
+  string getName() const { return name; }
+
+  void setAge(int a) { age = a; }
+  int getAge() const { return age; }
+
+  bool isOlder() const { return age > 17; }
+
+private:
+  string name;
+  int age;
+};
