e0a3b4865a44d267a17372c13a2ab02f52e268f1
Davkcy <164350888+Davkcy@users.noreply.github.com>
2024-04-04 07:43:42 -0500

    Update clases.h

--- clases.h
+++ clases.h
@@ -6,13 +6,23 @@ public:
   explicit Student(string n, int a);
   Student() : name(""), age(0) {}
 
-  void setName(string n) { name = n; }
-  string getName() const { return name; }
+  void setName(string n) { 
+    name = n; 
+  }
+  string getName() const { 
+    return name;
+  }
 
-  void setAge(int a) { age = a; }
-  int getAge() const { return age; }
+  void setAge(int a) { 
+    age = a;
+  }
+  int getAge() const { 
+    return age;
+  }
 
-  bool isOlder() const { return age > 17; }
+  bool isOlder() const { 
+    return age > 17;
+  }
 
 private:
   string name;
